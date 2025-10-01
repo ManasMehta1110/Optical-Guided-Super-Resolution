dfcff7acd114367db715d9df559ac9ce495f2caa
ManasMehta1110 <manasmehta1123@gmail.com>
2025-10-01 14:54:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 @st.cache_resource
 def load_model():
     model = DualEDSR().to(DEVICE)
-    checkpoint = torch.load(MODEL_PATH, map_location=DEVICE, weights_only=False)
+    checkpoint = torch.load(MODEL_PATH, map_location=DEVICE, weights_only=True)
     model.load_state_dict(checkpoint["model"])
     model.eval()
     return model
